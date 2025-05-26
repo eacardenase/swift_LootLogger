877741c79243baa3abab80a02efeeadc97f7b52f
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-26 16:46:26 -0500

    feat: add item model and designated initializer

---
+++ LootLogger/Item.swift
@@ -0,0 +1,22 @@
+//
+//  Item.swift
+//  LootLogger
+//
+//  Created by Edwin Cardenas on 5/26/25.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+    }
+}
