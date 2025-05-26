00caad0ab51db55b5650d2eae893cda97b5f839a
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-26 16:51:30 -0500

    feat: add item convenience initializer

--- LootLogger/Item.swift
+++ LootLogger/Item.swift
@@ -19,4 +19,24 @@ class Item {
         self.serialNumber = serialNumber
         self.dateCreated = Date()
     }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            let randomAdjective = adjectives.randomElement()!
+            let randomNoun = nouns.randomElement()!
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int.random(in: 0..<100)
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName,
+                      serialNumber: randomSerialNumber,
+                      valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
 }
