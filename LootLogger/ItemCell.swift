cccb50f8fc1e108210ed5214260e3a952ebe17af
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-31 20:28:48 -0500

    refactor: format item cell file

--- LootLogger/ItemCell.swift
+++ LootLogger/ItemCell.swift
@@ -8,40 +8,40 @@
 import UIKit
 
 class ItemCell: UITableViewCell {
-    
+
     let nameLabel: UILabel = {
         let label = UILabel()
-        
+
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .preferredFont(forTextStyle: .body)
         label.numberOfLines = 0
-        
+
         return label
     }()
     let serialNumberLabel: UILabel = {
         let label = UILabel()
-        
+
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .secondaryLabel
         label.font = .preferredFont(forTextStyle: .caption1)
-        
+
         return label
     }()
     let valueLabel: UILabel = {
         let label = UILabel()
-        
+
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .preferredFont(forTextStyle: .body)
-        
+
         return label
     }()
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+
         setupViews()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -49,25 +49,50 @@ class ItemCell: UITableViewCell {
 
 extension ItemCell {
     func setupViews() {
-        let stackView = UIStackView(arrangedSubviews: [nameLabel, serialNumberLabel])
-        
+        let stackView = UIStackView(arrangedSubviews: [
+            nameLabel, serialNumberLabel,
+        ])
+
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.distribution = .equalCentering
         stackView.spacing = 4
-        
+
         contentView.addSubview(stackView)
         contentView.addSubview(valueLabel)
-        
+
+        // stackView
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-            stackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
-            stackView.trailingAnchor.constraint(lessThanOrEqualTo: valueLabel.leadingAnchor, constant: 8),
-            
-            valueLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            valueLabel.centerYAnchor.constraint(equalTo: stackView.centerYAnchor)
+            stackView.topAnchor.constraint(
+                equalTo: contentView.topAnchor,
+                constant: 8
+            ),
+            stackView.leadingAnchor.constraint(
+                equalTo: contentView.leadingAnchor,
+                constant: 20
+            ),
+            stackView.centerYAnchor.constraint(
+                equalTo: contentView.centerYAnchor
+            ),
+            stackView.bottomAnchor.constraint(
+                equalTo: contentView.bottomAnchor,
+                constant: -8
+            ),
+            stackView.trailingAnchor.constraint(
+                lessThanOrEqualTo: valueLabel.leadingAnchor,
+                constant: 8
+            ),
+        ])
+
+        // valueLabel
+        NSLayoutConstraint.activate([
+            valueLabel.trailingAnchor.constraint(
+                equalTo: contentView.trailingAnchor,
+                constant: -20
+            ),
+            valueLabel.centerYAnchor.constraint(
+                equalTo: stackView.centerYAnchor
+            ),
         ])
     }
 }
