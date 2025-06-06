be7be2ba3469bd49b8c4c560fcf08cd3da92bd75
Edwin Cardenas <ecardenas@lwolf.com>
2025-06-06 14:00:04 -0500

    feat: add codable conformance to item manually

--- LootLogger/Item.swift
+++ LootLogger/Item.swift
@@ -8,10 +8,24 @@
 import Foundation
 
 class Item: Equatable, Codable {
+
+    enum Category {
+        case electronics, clothing, book, other
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case valueInDollars
+        case serialNumber
+        case dateCreated
+        case category
+    }
+
     var name: String
     var valueInDollars: Int
     var serialNumber: String
     let dateCreated: Date
+    var categoty = Category.other
 
     init(name: String, serialNumber: String, valueInDollars: Int) {
         self.name = name
@@ -49,5 +63,52 @@ class Item: Equatable, Codable {
             && lhs.serialNumber == rhs.serialNumber
             && lhs.valueInDollars == rhs.valueInDollars
             && lhs.dateCreated == rhs.dateCreated
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(name, forKey: .name)
+        try container.encode(valueInDollars, forKey: .valueInDollars)
+        try container.encode(serialNumber, forKey: .serialNumber)
+        try container.encode(dateCreated, forKey: .dateCreated)
+
+        switch categoty {
+        case .electronics:
+            try container.encode("electronics", forKey: .category)
+        case .clothing:
+            try container.encode("clothing", forKey: .category)
+        case .book:
+            try container.encode("book", forKey: .category)
+        case .other:
+            try container.encode("other", forKey: .category)
+        }
+    }
+
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        name = try container.decode(String.self, forKey: .name)
+        valueInDollars = try container.decode(Int.self, forKey: .valueInDollars)
+        serialNumber = try container.decode(String.self, forKey: .serialNumber)
+        dateCreated = try container.decode(Date.self, forKey: .dateCreated)
+
+        let categoryString = try container.decode(
+            String.self,
+            forKey: .category
+        )
+
+        switch categoryString {
+        case "electronics":
+            categoty = .electronics
+        case "clothing":
+            categoty = .clothing
+        case "book":
+            categoty = .book
+        case "other":
+            categoty = .other
+        default:
+            categoty = .other
+        }
     }
 }
